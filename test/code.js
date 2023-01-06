d7ca6665f1d224f28ad2bf464a924488f8a0be8b
David Furcy <furcyd@uwosh.edu>
2023-01-06 14:23:07 -0600

    Attempt ro prevent scrolling when dragging in puzzle

--- test/code.js
+++ test/code.js
@@ -216,8 +216,15 @@ var BSP = {};
     {
     }
 
+    function preventDefault(e) {
+        e = e || window.event;
+        if (e.preventDefault) e.preventDefault();
+        e.returnValue = false;
+    }
+    
     function showPuzzle(level,number)
     {
+	window.ontouchmove = preventDefault;
 	number = Number(number);
 	numberSteps = 0;
 	console.log("Puzzle " + level + number);
